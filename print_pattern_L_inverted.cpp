6831d5909ff6841bd05e5aaee051b5f557fcb2dc
Bharathlakshmi <bharathlakshmiselvaraj11@gmail.com>
2025-01-11 12:19:52 +0530

    added new program: Pattern Printing

---
+++ print_pattern_L_inverted.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+    int n,p;
+    cout<<"Enter no. of rows : ";
+    cin>>n;
+    p=n;
+
+    for(int i=0;i<n;i++)
+    {
+        for(int j=p;j>0;j--)
+        {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+        p--;
+    }
+}
+
